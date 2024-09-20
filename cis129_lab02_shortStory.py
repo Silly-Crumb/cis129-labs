f9ebaa0183dd8995d29a3d9ad6f3f349d80bcc4e
Silly-Crumb <csorensen4@mail.pima.edu>
2024-09-19 22:45:49 -0700

    Update cis129_lab02_shortStory.py
    deleted the print for hello world. added print statements to print a story about a man who gets bread and butter.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+#Author: Student Catherine
+#Python lab for CIS129
+#added print statements about a man with his bread and butter
+print('A man goes to the market for some bread and butter')
+print('He finds the stall for bread then pays for it and continues')
+print('He then finds the dairy stall and buys his butter')
+print('He goes back home with his bread and butter content')
